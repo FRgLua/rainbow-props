7eab954652a6147c4b39819984ccfcbb99272e36
FRgLua <77032726+FRgLua@users.noreply.github.com>
2021-01-07 12:17:02 -0300

    Add files via upload

---
+++ RainbowProps.lua
@@ -0,0 +1,15 @@
+function testing(ply , ent)
+
+	local r = math.random(1,255)
+	local g = math.random(1,255)
+	local b = math.random(1,255)
+
+	for k ,v in pairs(ents.GetAll()) do
+
+		v:SetColor(Color(r,g,b))
+
+	end
+
+end
+
+hook.Add("Think" , "UnquieName" , testing) 
